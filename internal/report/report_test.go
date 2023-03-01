bde27a05321527d8aade0cb471e853c1c750269f
Adan J. Suarez <adanjsuarez@gmail.com>
2023-03-01 17:55:21 -0500

    feat: added more tests to report

--- internal/report/report_test.go
+++ internal/report/report_test.go
@@ -26,49 +26,79 @@ func (ts *TSReport) BeforeTest(_, _ string) {
 }
 
 func (ts *TSReport) TestShouldExcludeTrue1() {
-	logTrue1 := "/7b0744/css/vegguide-combined.css"
-	actual := reportTest.shouldExclude(logTrue1)
+	logTrue := "/7b0744/css/vegguide-combined.css"
+	actual := reportTest.shouldExclude(logTrue)
 	ts.True(actual)
 }
 
 func (ts *TSReport) TestShouldExcludeTrue2() {
-	logTrue2 := "/7b0744/css/vegguide-combined"
-	actual := reportTest.shouldExclude(logTrue2)
+	logTrue := "/7b0744/css/vegguide-combined"
+	actual := reportTest.shouldExclude(logTrue)
 	ts.True(actual)
 }
 
 func (ts *TSReport) TestShouldExcludeFalse3() {
-	logFalse3 := "/7b0744/csst/vegguide-combined"
-	actual := reportTest.shouldExclude(logFalse3)
+	logFalse := "/7b0744/csst/vegguide-combined"
+	actual := reportTest.shouldExclude(logFalse)
 	ts.False(actual)
 }
 
 func (ts *TSReport) TestShouldExcludeFalse4() {
-	logFalse4 := "/7b0744/cs/vegguide-combined"
-	actual := reportTest.shouldExclude(logFalse4)
+	logFalse := "/7b0744/cs/vegguide-combined"
+	actual := reportTest.shouldExclude(logFalse)
 	ts.False(actual)
 }
 
 func (ts *TSReport) TestShouldExcludeTrue5() {
-	logTrue5 := "/7b0744/cs/vegguide-combined.rss"
-	actual := reportTest.shouldExclude(logTrue5)
+	logTrue := "/7b0744/cs/vegguide-combined.rss"
+	actual := reportTest.shouldExclude(logTrue)
 	ts.True(actual)
 }
 
 func (ts *TSReport) TestShouldExcludeTrue6() {
-	logTrue6 := "/7b0744/cs/vegguide-combined.rss/"
-	actual := reportTest.shouldExclude(logTrue6)
+	logTrue := "/7b0744/cs/vegguide-combined.rss/"
+	actual := reportTest.shouldExclude(logTrue)
 	ts.True(actual)
 }
 
 func (ts *TSReport) TestShouldExcludeFalse7() {
-	logFalse7 := "/7b0744/cs/vegguide-combined.rs"
-	actual := reportTest.shouldExclude(logFalse7)
+	logFalse := "/7b0744/cs/vegguide-combined.rs"
+	actual := reportTest.shouldExclude(logFalse)
 	ts.False(actual)
 }
 
 func (ts *TSReport) TestShouldExcludeFalse8() {
-	logFalse8 := "/7b0744/cs/vegguide-combined.rsss"
-	actual := reportTest.shouldExclude(logFalse8)
+	logFalse := "/7b0744/cs/vegguide-combined.rsss"
+	actual := reportTest.shouldExclude(logFalse)
+	ts.False(actual)
+}
+
+func (ts *TSReport) TestShouldExcludeTrue9() {
+	logTrue := "/images/ratings/blue-3-00.png"
+	actual := reportTest.shouldExclude(logTrue)
+	ts.True(actual)
+}
+
+func (ts *TSReport) TestShouldExcludeTrue10() {
+	logTrue := "/7b0744/images/ratings/green-0-00.png"
+	actual := reportTest.shouldExclude(logTrue)
+	ts.True(actual)
+}
+
+func (ts *TSReport) TestShouldExcludeFalse11() {
+	logFalse := "/image/ratings/blue-3-00.png"
+	actual := reportTest.shouldExclude(logFalse)
+	ts.False(actual)
+}
+
+func (ts *TSReport) TestShouldExcludeFalse12() {
+	logFalse := "images/ratings/blue-3-00.png"
+	actual := reportTest.shouldExclude(logFalse)
+	ts.False(actual)
+}
+
+func (ts *TSReport) TestShouldExcludeFalse13() {
+	logFalse := "/imagess/ratings/blue-3-00.png"
+	actual := reportTest.shouldExclude(logFalse)
 	ts.False(actual)
 }
