e16971fc3a1e58349b4af901e68e2aa2101f5245
Adan J. Suarez <adanjsuarez@gmail.com>
2023-03-01 18:25:23 -0500

    feat: Added webpage to logparser

--- internal/logparser/logparser_test.go
+++ internal/logparser/logparser_test.go
@@ -11,7 +11,7 @@ const (
 )
 
 var (
-	parserTest *LogParser
+	logParserTest *LogParser
 )
 
 type TSLogParser struct{ suite.Suite }
@@ -21,11 +21,11 @@ func TestRunTSLogParser(t *testing.T) {
 }
 
 func (ts *TSLogParser) BeforeTest(_, _ string) {
-	parserTest = New()
+	logParserTest = New()
 }
 
 func (ts *TSLogParser) TestLogLine() {
-	actual := parserTest.Parse(logTest)
+	actual := logParserTest.Parse(logTest)
 	ts.Equal("183.60.212.148", actual.IP)
 	ts.Equal("26/Aug/2014:06:26:39 -0600", actual.TS)
 	ts.Equal("GET", actual.RequestMethod)
@@ -36,11 +36,11 @@ func (ts *TSLogParser) TestLogLine() {
 }
 
 func (ts *TSLogParser) TestParseValidInteger() {
-	actual := parserTest.parseStringToInt64("333")
+	actual := logParserTest.parseStringToInt64("333")
 	ts.Equal(int64(333), actual)
 }
 
 func (ts *TSLogParser) TestParseInvalidInteger() {
-	actual := parserTest.parseStringToInt64("turbo")
+	actual := logParserTest.parseStringToInt64("turbo")
 	ts.Equal(int64(0), actual)
 }
