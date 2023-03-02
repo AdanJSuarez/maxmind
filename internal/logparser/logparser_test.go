4c57446845502ea921bb7b5b526b62a514fb9504
Adan J. Suarez <adanjsuarez@gmail.com>
2023-03-02 10:10:10 -0500

    Fixed logparser

--- internal/logparser/logparser_test.go
+++ internal/logparser/logparser_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 const (
-	logTest = `183.60.212.148 - - [26/Aug/2014:06:26:39 -0600] "GET /entry/15205 HTTP/1.1" 200 4865 "-" "Mozilla/5.0 (compatible; EasouSpider; +http://www.easou.com/search/spider.html)"`
+	logTest1           = `183.60.212.148 - - [26/Aug/2014:06:26:39 -0600] "GET /entry/15205 HTTP/1.1" 200 4865 "-" "Mozilla/5.0 (compatible; EasouSpider; +http://www.easou.com/search/spider.html)"`
+	logTest2           = `68.180.225.35 - - [26/Aug/2014:06:44:30 -0600] "GET /entry/20153 HTTP/1.1" 200 4539 "-" "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)"`
+	logIncompleteTest3 = `23.239.8.38 - - [26/Aug/2014:06:59:31 -0600] "GET / HTTP/1.1" 301 178`
+	logCorruptTest4    = `😀😁`
 )
 
 var (
@@ -21,18 +24,42 @@ func TestRunTSLogParser(t *testing.T) {
 }
 
 func (ts *TSLogParser) BeforeTest(_, _ string) {
-	logParserTest = New()
+	var err error
+	logParserTest, err = New()
+	ts.NoError(err)
 }
 
-func (ts *TSLogParser) TestLogLine() {
-	actual := logParserTest.Parse(logTest)
+func (ts *TSLogParser) TestLogLine1() {
+	actual, err := logParserTest.Parse(logTest1)
+	ts.NoError(err)
 	ts.Equal("183.60.212.148", actual.IP)
 	ts.Equal("26/Aug/2014:06:26:39 -0600", actual.TS)
 	ts.Equal("GET", actual.RequestMethod)
 	ts.Equal("/entry/15205", actual.RequestPath)
 	ts.Equal(int64(200), actual.StatusCode)
 	ts.Equal(int64(4865), actual.Size)
-	ts.Equal(actual.WebPage, "http://www.easou.com/search/spider.html")
+}
+
+func (ts *TSLogParser) TestLogLine2() {
+	actual, err := logParserTest.Parse(logTest2)
+	ts.NoError(err)
+	ts.Equal("68.180.225.35", actual.IP)
+	ts.Equal("26/Aug/2014:06:44:30 -0600", actual.TS)
+	ts.Equal("GET", actual.RequestMethod)
+	ts.Equal("/entry/20153", actual.RequestPath)
+	ts.Equal(int64(200), actual.StatusCode)
+	ts.Equal(int64(4539), actual.Size)
+}
+
+func (ts *TSLogParser) TestLogLine3() {
+	actual, err := logParserTest.Parse(logIncompleteTest3)
+	ts.ErrorContains(err, "does not have all the matches")
+	ts.Empty(actual)
+}
+func (ts *TSLogParser) TestLogLine4() {
+	actual, err := logParserTest.Parse(logCorruptTest4)
+	ts.ErrorContains(err, "does not have all the matches")
+	ts.Empty(actual)
 }
 
 func (ts *TSLogParser) TestParseValidInteger() {
