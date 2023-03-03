a54a9ba12affa3e9df36281102ff3b7c8a0930d3
Adan J. Suarez <adanjsuarez@gmail.com>
2023-03-03 15:15:42 -0500

    test: Added first tests to GeoInfoModel

--- internal/geoinfo/geoinfomodel_test.go
+++ internal/geoinfo/geoinfomodel_test.go
@@ -3,10 +3,36 @@ package geoinfo
 import (
 	"testing"
 
+	"github.com/oschwald/geoip2-golang"
 	"github.com/stretchr/testify/suite"
 )
 
-var geoInfoModelTest GeoInfoModel
+var (
+	geoInfoModelTest GeoInfoModel
+	recordTest1      = geoip2.City{
+		Country: struct {
+			GeoNameID         uint              `maxminddb:"geoname_id"`
+			IsInEuropeanUnion bool              `maxminddb:"is_in_european_union"`
+			IsoCode           string            `maxminddb:"iso_code"`
+			Names             map[string]string `maxminddb:"names"`
+		}{
+			Names: map[string]string{"en": "Spain"},
+		},
+		Subdivisions: []struct {
+			GeoNameID uint              `maxminddb:"geoname_id"`
+			IsoCode   string            `maxminddb:"iso_code"`
+			Names     map[string]string `maxminddb:"names"`
+		}{
+			struct {
+				GeoNameID uint              "maxminddb:\"geoname_id\""
+				IsoCode   string            "maxminddb:\"iso_code\""
+				Names     map[string]string "maxminddb:\"names\""
+			}{
+				Names: map[string]string{"en": "Tenerife"},
+			},
+		},
+	}
+)
 
 type TSGeoInfoModel struct{ suite.Suite }
 
@@ -15,7 +41,18 @@ func TestRunTSGeoInfoModel(t *testing.T) {
 }
 
 func (ts *TSGeoInfoModel) BeforeTest(_, _ string) {
-	geoInfoModelTest = newGeoInfoModel("", nil)
+	geoInfoModelTest = newGeoInfoModel("122.122.12.22", nil)
 }
 
-func (ts *TSGeoInfoModel) Test() {}
+func (ts *TSGeoInfoModel) TestGeoInfoModelInitialized() {
+	ts.Equal("122.122.12.22", geoInfoModelTest.IP)
+	ts.Equal(unknown, geoInfoModelTest.CountryName)
+	ts.Equal(unknown, geoInfoModelTest.Subdivisions[0])
+}
+
+func (ts *TSGeoInfoModel) TestGeoInfoModelInitialized2() {
+	geoInfoModelTest = newGeoInfoModel("122.122.12.23", &recordTest1)
+	ts.Equal("122.122.12.23", geoInfoModelTest.IP)
+	ts.Equal("Spain", geoInfoModelTest.CountryName)
+	ts.Equal("Tenerife", geoInfoModelTest.Subdivisions[0])
+}
