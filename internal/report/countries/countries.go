1989df6249d43a2a6844216663585856355725eb
Adan J. Suarez <adanjsuarez@gmail.com>
2023-03-06 11:06:07 -0500

    feat: finished features

--- internal/report/countries/countries.go
+++ internal/report/countries/countries.go
@@ -1,6 +1,19 @@
 package countries
 
-import "github.com/AdanJSuarez/maxmind/internal/node"
+import (
+	"github.com/AdanJSuarez/maxmind/internal/node"
+)
+
+const (
+	top          = 10
+	unitedStates = "United States"
+)
+
+type Info struct {
+	Name    string
+	Visit   int64
+	TopPage string
+}
 
 type Countries struct {
 	countries *node.Node
@@ -16,6 +29,30 @@ func (c *Countries) AddToCountries(countryName, subdivisionName, webpageName str
 	c.countries.AddToNode(countryName, subdivisionName, webpageName)
 }
 
-func (c *Countries) Countries() map[string]*node.Node {
-	return c.countries.Children()
+func (c *Countries) Countries() *node.Node {
+	return c.countries
+}
+
+func (c *Countries) TopPages(excluded string) []node.Data {
+	return c.countries.SortedData(excluded)[:top]
+}
+
+func (c *Countries) TopAreas(areas *node.Node, exclude string) []Info {
+	var result []Info
+	for _, val := range areas.SortedChildrenByCounter() {
+		area, found := areas.Children()[val.Key()]
+		if !found {
+			continue
+		}
+		areaInfo := Info{
+			Name:    val.Key(),
+			Visit:   val.Value(),
+			TopPage: area.SortedData(exclude)[0].Key(),
+		}
+		result = append(result, areaInfo)
+		if len(result) == 10 {
+			break
+		}
+	}
+	return result
 }
--- internal/report/report.go
+++ internal/report/report.go
@@ -1,6 +1,7 @@
 package report
 
 import (
+	"fmt"
 	"log"
 	"regexp"
 	"sync"
@@ -61,7 +62,6 @@ func (r *Report) GetReport() {
 		}
 		record := r.geoInfo.GetIPInfo(lineLog.IP)
 		// log.Println("Info ==> ", record)
-		// TODO: Check which subscription to pick.
 		r.countries.AddToCountries(record.CountryName, record.Subdivisions[0], lineLog.RequestPath)
 
 	}
@@ -73,12 +73,13 @@ func (r *Report) shouldExclude(requestPath string) bool {
 }
 
 func (r *Report) printReport() {
-	// for idx, country := range r.countries.Countries() {
-	// fmt.Printf("Country: %s - counter :%d\n", idx, country.Counter())
-	// fmt.Printf("Number: %d: %s - Counter: %d\n", idx+1, country.Name(), country.Counter())
-	// webpage, numberOfVisit := country.MostVisitWebpage("")
-	// fmt.Printf("-----> Most visited webpage: %s - visits: %d\n", webpage, numberOfVisit)
-	// fmt.Println("")
-	// }
+	fmt.Println("Countries:")
+	for idx, val := range r.countries.TopAreas(r.countries.Countries(), "/") {
+		fmt.Printf("%d : %s - Visits: %d - Most visited page: %s\n", idx, val.Name, val.Visit, val.TopPage)
+	}
+	fmt.Println("United States:")
+	for _, val := range r.countries.TopAreas(r.countries.Countries().Children()["United States"], "/") {
+		fmt.Printf("State: %s - Visits: %d - Most visited page: %s\n", val.Name, val.Visit, val.TopPage)
+	}
 	log.Println("==> Finished <== ")
 }
